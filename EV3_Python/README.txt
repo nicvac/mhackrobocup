cbc9f35739f1c86b63d9953da3a4ce64951cd2ca
Alessandro-sys <71657415+Alessandro-sys@users.noreply.github.com>
2023-05-05 16:03:09 +0200

    modifiche

--- EV3_Python/README.txt
+++ EV3_Python/README.txt
@@ -20,14 +20,14 @@ MOTORI:
 gira 90 gradi
 
 robot.settings(30, 100, 90)
-primo argomento = distanza sul dritto
+primo argomento = potenza sul dritto
 secondo argomento = accelerazione sul dritto
-terzo argomento = angolo curva
+terzo argomento = potenza curva
 quarto argomento = accelerazione curva
 
 
 robot.turn(60)
-argomento = potenza di curva
+argomento = angolo di curva
 
 robot.stop()
 per resettare le impostazioni e poterle cambiare
--- EV3_Python/prova.py
+++ EV3_Python/prova.py
@@ -15,10 +15,10 @@ wheel = 30
 axle = 160
 
 # Motore grande destra
-right_motor = Motor(Port.A, positive_direction=Direction.CLOCKWISE)
+right_motor = Motor(Port.A)
 
 # Motore grande sinistra
-left_motor = Motor(Port.B, positive_direction=Direction.CLOCKWISE)
+left_motor = Motor(Port.B)
 
 # Sensore di colore destra
 color_sensor_right = ColorSensor(Port.S2)
@@ -27,16 +27,18 @@ color_sensor_right = ColorSensor(Port.S2)
 color_sensor_left = ColorSensor(Port.S3)
 
 # Sensore in luce riflessa fronte
-light_sensor_front = ColorSensor(Port.S4)
+#light_sensor_front = ColorSensor(Port.S4)
 
 #Sensore in colore trasformato fronte
-color_sensor_front = ColorSensor(Port.S4)
+#color_sensor_front = ColorSensor(Port.S4)
 
 # Sensore ultrasuoni
 # ultrasonic_sensor = UltrasonicSensor(Port.S1)
 
 # Configurazione robot
 robot = DriveBase(left_motor, right_motor, wheel_diameter=wheel, axle_track=axle)
+
+maxpower = 1020 
 
 
 
@@ -56,19 +58,48 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=wheel, axle_track=axle
 # while True:
 #     left_motor.run(300)
 #     right_motor.run(300)
-
+# stampa = True
 
 # while True:
-#     if color_sensor_left.color() == Color.BLACK:
-#         # left_motor.run(-700)
-#         # right_motor.run(500)
 
-#         robot.drive(500, 30)
-#     elif color_sensor_right.color() == Color.BLACK:
-#         right_motor.run(-700)
-#         left_motor.run(500)
-#     else:
-#         left_motor.run(400)
-#         right_motor.run(400)
+    
 
-robot.drive(500, 30)
+#     colorl = color_sensor_left.color()
+    
+#     colorr = color_sensor_right.color()
+    
+#     if stampa == True:
+#         print(right_motor.speed())
+#         print(colorl)
+#         print(colorr)
+    
+#     if colorl == Color.BLACK:
+#         left_motor.hold()
+#         while color_sensor_left.color() == Color.BLACK:
+#             left_motor.run(-408)
+#             right_motor.run(510)
+
+#     elif colorr == Color.BLACK:
+#         right_motor.hold()
+#         while color_sensor_right.color() == Color.BLACK:
+#             right_motor.run(-408)
+#             left_motor.run(510)
+
+#     # elif colorr == Color.WHITE and colorl == Color.WHITE and
+
+#     else: # colorl != Color.BLACK and colorr != Color.BLACK:
+
+#         left_motor.run(408)
+#         right_motor.run(408)
+
+#     stampa = not stampa
+
+
+
+
+
+left_motor.dc(100)
+right_motor.dc(-100)
+
+while True:
+    pass
