d454f4113fd3e17d21b746468b1c014d51c70645
nicvac <nicola.vacca@gmail.com>
2023-05-13 17:42:00 +0200

    Minor

--- EV3_Python/Caricare/stampa_sensori.py
+++ EV3_Python/Caricare/stampa_sensori.py
@@ -5,15 +5,17 @@ from resque_line_functions import *
 
 import time
 
+#Stampa sensori
 # while True:  
 #     print(color_sensor_left.color(), " - ", color_sensor_right.color(), "\t\t F:", light_sensor_front.reflection() )
 #     #print(light_sensor_front.reflection())
 #     time.sleep(0.5)
 
 
-lineLocked = False
-scanDegree = 100
-while not lineLocked:
-    robot.straight(50) #5 cm
-    robot.stop()
-    lineLocked = scan_double(scanDegree, 0)
+#Test scan
+# lineLocked = False
+# scanDegree = 100
+# while not lineLocked:
+#     robot.straight(50) #5 cm
+#     robot.stop()
+#     lineLocked = scan_double(scanDegree, 0)
