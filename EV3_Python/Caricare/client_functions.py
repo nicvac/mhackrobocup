fbb90c4ac4dd66a25c0e6f9fa6eb92c12c1c4090
nicvac <nicola.vacca@gmail.com>
2023-05-25 13:41:17 +0200

    Client Server: Metodo Broadcast

--- EV3_Python/Caricare/client_functions.py
+++ EV3_Python/Caricare/client_functions.py
@@ -1,12 +1,16 @@
+# Messaging
+# https://pybricks.com/ev3-micropython/messaging.html
+# ATTENZIONE: USANDO IL MECCANISMO DI WAIT REQUEST DAL CLIENT, SI RISCHIA DI PERDERSI LA SINCRONIA 
+#  FRA SERVERE E CLIENT, SERVER PERDE LA RICHIESTA DEL CLIENT E RIMANE INDEFINITIVAMENTE IN WAIT.
+#  QUINDI RACCOLGO I DATI DAL SENSORE E LI SPARO A RIPETIZIONE. SARA' SOLO IL CLIENT A FARE WAIT 
+#  QUANDO GLI SERVE
+
 from pybricks.messaging import BluetoothMailboxClient, TextMailbox, NumericMailbox
 
 #Il brick server (fornisce le distanze)
 SERVER = 'ev3devExt'
 
-#Messaggio per le distanze
-mailbox_extDist = 'extDist'
-
-#Porte dei sensori sul server
+#Tipo di sensore di distanza
 DIST_FRONT = 1
 DIST_BACK = 4
 DIST_LEFT = 2
@@ -14,24 +18,36 @@ DIST_RIGHT = 3
 
 #Ritorna la distanza del sensore server in cm
 def getDistanceMM(sensore):
-        extDist = NumericMailbox( mailbox_extDist, client)
-        extDist.send(sensore)
-        extDist.wait()
-        dist_cm = extDist.read()
-        return dist_cm
+    if sensore == DIST_FRONT:
+        extDistFront.wait()
+        dist_cm = extDistFront.read()
+    elif sensore == DIST_BACK:
+        extDistBack.wait()
+        dist_cm = extDistBack.read()
+    elif sensore == DIST_LEFT:
+        extDistLeft.wait()
+        dist_cm = extDistLeft.read()
+    elif sensore == DIST_RIGHT:
+        extDistRight.wait()
+        dist_cm = extDistRight.read()
+    return dist_cm
 
 #Ritorna la distanza in cm, con contatore di stabilità
 def getDistanceCm(sensore):
-    curr = getDistanceMM(sensore) / 10
-    #print(curr)
-    return curr
+    dist_cm = getDistanceMM(sensore) / 10
+    #print(dist_cm)
+    return dist_cm
 
 print("OPERAZIONE DI CONNESSIONE AL SERVER AVVIATA, COMMENTARE SE NON SERVE")
 
-
 client = BluetoothMailboxClient()
-#mbox = TextMailbox('greeting', client)
 
 print('establishing connection...')
 client.connect(SERVER)
 print('connected!')
+
+#USARE LE STESSE ETICHETTE DEL SERVER
+extDistFront = NumericMailbox('extDistFront', client)
+extDistBack  = NumericMailbox('extDistBack', client)
+extDistLeft  = NumericMailbox('extDistLeft', client)
+extDistRight  = NumericMailbox('extDistRight', client)
